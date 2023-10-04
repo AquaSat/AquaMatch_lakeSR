fd0b66478409751296b80aed8551df011a398d11
B Steele <B.Steele@colostate.edu>
2023-10-04 16:45:11 -0600

    start moving over Landsat pull

--- _targets.R
+++ _targets.R
@@ -1,18 +1,29 @@
 # Load packages required to define the pipeline:
 library(targets)
 library(tarchetypes) 
+library(reticulate)
 
-# Set target options:
+# Set up python virtual environment ---------------------------------------
+
+if (!dir.exists("env")) {
+  tar_source("py/pySetup.R")
+} else {
+  use_condaenv(file.path(getwd(), "env"))
+}
+
+# Set target options: ---------------------------------------
+
 tar_option_set(
   packages = c("tidyverse") # packages that your targets need to run
 )
 
+# Source targets groups: ---------------------------------------
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source(
   "a_Calculate_Centers.R"
 )
 
-# Replace the target list below with your own:
+# Collate targets groups: ---------------------------------------
 list(
   a_Calculate_Centers_list
 )
