eddc0b4ab3f872f1e234a2fa48cde2d68ce58dbb
B Steele <B.Steele@colostate.edu>
2023-10-04 16:44:13 -0600

    update assignments

--- a_Calculate_Centers/src/calculate_centers_HUC4.R
+++ a_Calculate_Centers/src/calculate_centers_HUC4.R
@@ -82,35 +82,41 @@ calculate_centers_HUC4 <- function(HUC4) {
       )
       for (i in 1:length(wbd[[1]])) {
         poi_df  <- poi_df %>% add_row()
-        one_wbd = wbd[i,]
-        # get coordinates to calculate UTM zone
-        coord_for_UTM = one_wbd %>% st_coordinates()
-        mean_x = mean(coord_for_UTM[,1])
-        mean_y = mean(coord_for_UTM[,2])
-        utm_suffix = as.character(ceiling((mean_x + 180) / 6))
-        utm_code = if_else(mean_y >= 0,
+        one_wbd <- wbd[i,]
+        # get coordinates to calculate UTM zone. This is an adaptation of code from
+        # Xiao Yang's code in EE - Yang, Xiao. (2020). Deepest point calculation 
+        # for any given polygon using Google Earth Engine JavaScript API 
+        # (Version v 1). Zenodo. https://doi.org/10.5281/zenodo.4136755
+        coord_for_UTM <- one_wbd %>% st_coordinates()
+        mean_x <- mean(coord_for_UTM[,1])
+        mean_y <- mean(coord_for_UTM[,2])
+        # calculate the UTM zone using the mean value of Longitude for the polygon
+        utm_suffix <- as.character(ceiling((mean_x + 180) / 6))
+        utm_code <- if_else(mean_y >= 0,
+                           # EPSG prefix for N hemisphere
                            paste0('EPSG:326', utm_suffix),
+                           # for S hemisphere
                            paste0('EPSG:327', utm_suffix))
         # transform wbd to UTM
-        one_wbd_utm = st_transform(one_wbd, 
+        one_wbd_utm <- st_transform(one_wbd, 
                                    crs = utm_code)
         # get UTM coordinates
-        coord = one_wbd_utm %>% st_coordinates()
-        x = coord[,1]
-        y = coord[,2]
+        coord <- one_wbd_utm %>% st_coordinates()
+        x <- coord[,1]
+        y <- coord[,2]
         # using coordinates, get the poi distance
-        poly_poi = poi(x,y, precision = 0.01)
+        poly_poi <- poi(x,y, precision = 0.01)
         # add info to poi_df
         poi_df$rowid[i] = wbd[i,]$rowid
         poi_df$Permanent_Identifier[i] = as.character(wbd[i,]$Permanent_Identifier)
         poi_df$poi_dist_m[i] = poly_poi$dist
         # make a point feature and re-calculate decimal degrees in WGS84
-        point = st_point(x = c(as.numeric(poly_poi$x),
+        point <- st_point(x = c(as.numeric(poly_poi$x),
                                as.numeric(poly_poi$y)))
-        point = st_sfc(point, crs = utm_code)
-        point = st_transform(st_sfc(point), crs = 'EPSG:4326')
+        point <- st_sfc(point, crs = utm_code)
+        point <- st_transform(st_sfc(point), crs = 'EPSG:4326')
                           
-        new_coords = point %>% st_coordinates()
+        new_coords <- point %>% st_coordinates()
         poi_df$poi_Longitude[i] = new_coords[,1]
         poi_df$poi_Latitude[i] = new_coords[,2]
       }
