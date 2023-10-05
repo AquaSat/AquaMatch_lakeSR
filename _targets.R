50847b41b57af9fc51cd0251f8c676920f8780f4
B Steele <B.Steele@colostate.edu>
2023-10-05 13:52:45 -0600

    re-render with edits

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@ library(reticulate)
 # Set up python virtual environment ---------------------------------------
 
 if (!dir.exists("env")) {
-  tar_source("py/pySetup.R")
+  tar_source("python/pySetup.R")
 } else {
   use_condaenv(file.path(getwd(), "env"))
 }
@@ -17,13 +17,21 @@ tar_option_set(
   packages = c("tidyverse") # packages that your targets need to run
 )
 
+# Point to config files: ---------------------------------------
+
+poi_config <- "config_files/config_poi.yml"
+
 # Source targets groups: ---------------------------------------
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source(
   "a_Calculate_Centers.R"
+  # ,
+  # "b_pull_Landsat_SRST_poi.R"
 )
 
 # Collate targets groups: ---------------------------------------
 list(
   a_Calculate_Centers_list
+  # ,
+  # b_pull_Landsat_SRST_poi_list
 )
