1108767ae04e8787f32ba458d505744d00642305
B Steele <B.Steele@colostate.edu>
2024-07-18 15:19:16 -0600

    work on poi/dp comparison

--- _targets.R
+++ _targets.R
@@ -36,7 +36,11 @@ list(
   tar_target(name = render_bookdown,
              command = {
                poi_tasks_complete #this should always be the last target!
-               render_book(input = "bookdown/")
+               render_book(input = "bookdown/",
+                           params = list(
+                             all_pts = combined_poi_points,
+                             ak_pts = AK_poi_points
+                           ))
                },
              packages = "bookdown")
 )
