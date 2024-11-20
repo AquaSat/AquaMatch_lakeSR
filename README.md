7738b5025a81d27beba18dea5f606388dccb3723
B Steele <B.Steele@colostate.edu>
2024-11-20 12:38:17 -0700

    pull scripts from main branch for PR

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# lakeSR
+# AquaMatch_lakeSR
 
-Repository to acquire, collate, and baseline QAQC satellite surface reflectance 
-data for all lakes \>1ha in the United States.
+Repository to acquire, collate, and baseline QAQC Landsat Collection 2 Surface 
+Reflectance Product for all lakes/reservoirs/impoundments \>1ha in the United States
+and Territories. This workflow is part of the AquaMatch dataset, an updated version
+of [AquaSat](https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2019WR024883) 
+with more robust QAQC measures and added documentation and modularity. 
 
-This repository is covered by the MIT use license. We request that all downstream 
-uses of this work be available to the public when possible.
+The code in this repository is covered by the MIT use license. We request that 
+all downstream uses of this work be available to the public when possible.
 
 Repository contact: B Steele (b dot steele at colostate dot edu)
 
 ## Targets Architecture
 
 The {targets} workflow defined in this repository acquires, collates, and performs 
-baseline QAQC for Landsat Collection 2 Surface Reflectance data for all 
-lakes/reservoirs/impoundments greater than 1 hectare for Landsat 4 through 9. 
+baseline QAQC for Landsat Collection 2 Surface Reflectance data for non-intermittent 
+lakes/reservoirs/impoundments greater than 1 hectare for Landsat 4 through 9 and
+intermittent lakes/reservoirs/impoundments greater than 4 hectares. 
 The architecture is broken up into grouped lists by function, those groups are 
 listed below with a description of what each group does.
 
@@ -20,11 +24,12 @@ listed below with a description of what each group does.
 
 This {targets} list calculates "Point of Inaccessibility", also known as Cheybyshev 
 Center for all lakes/reservoirs/impoundments greater than 1ha in surface area 
-using the NHDPlusHR polygons using the {nhdplusTools} package and the `poi()` 
-function in the {polylabelr} package. For all waterbodies in Alaska, POI were 
-calculated based on the NHD Best Resolution file for the entire state because 
-the NHDPlusHR is not complete for AK. **Note**: this group of targets will take 
-up to 4h to complete.
+using the NHDPlus polygons using the {nhdplusTools} package and the `poi()` 
+function in the {polylabelr} package. Alaska, HI, and some other HUC4 waterbodies 
+are not included in the NHDPlusv2, so they are downloaded by url from The 
+National Map and are processed in a separate target. 
+
+**Note**: this group of targets will take up to 4h to complete.
 
 
 **b_pull_Landsat_SRST_poi**:
@@ -32,10 +37,9 @@ up to 4h to complete.
 This {targets} group uses the config file `config_files/config_poi.yml` and the 
 Chebyshev Center points created in the `a_Calculate_Centers` group to pull 
 Landsat Collection 2 Surface Reflectance and Surface Temperature using the GEE
-API. In this group, we use the most strict LS4-7 pixel filters which include
-the `sr_cloud_mask` filter. This filter is a conservative filter, removing 
-artefacts from upstream products that are used to create the SR product. This 
-group of targets ends with a branched target that maps over each of the WRS2
-path rows that intersect with the points. **Note**: this group of targets takes
+API. This group of targets ends with a branched target that maps over each of the WRS2
+path rows that intersect with the points. 
+
+**Note**: this group of targets takes
 a very, very long time, ranging between 8 and 45 minutes per path row branch. 
 There are just under 800 path rows with points in them.
--- a_Calculate_Centers.R
+++ a_Calculate_Centers.R
@@ -4,26 +4,36 @@ tar_source("a_Calculate_Centers/src/")
 
 # Calculate POI Centers for all US and Territories Lakes -------------
 
-# This {targets} list calculates "Point of Inaccessibility", also known as
-# Cheybyshev Center for all lakes/reservoirs/impoundments greater than 1ha in 
-# surface area using the NHDPlusHR polygons using the {nhdplusTools} package
-# and the `poi()` function in the {polylabelr} package. For all waterbodies in
-# Alaska, POI were calculated based on the NHD Best Resolution file for the entire
-# state because the NHDPlusHR is not complete for AK. **Note**: this group of
-# targets will take up to 4h to complete.
-
-# create folder structure
-suppressWarnings({
-  dir.create("a_Calculate_Centers/mid/")
-  dir.create("a_Calculate_Centers/multisurface/")
-  dir.create("a_Calculate_Centers/out/")
-  dir.create("a_Calculate_Centers/nhd/")
-})
-
+# This {targets} list calculates "Point of Inaccessibility", also known as Cheybyshev 
+# Center for all lakes/reservoirs/impoundments greater than 1ha in surface area 
+# using the NHDPlus polygons using the {nhdplusTools} package and the `poi()` 
+# function in the {polylabelr} package. At some point, this workflow will need
+# to be updated to the new USGS 3DHP data, but that isn't complete right now. 
+# Additionally, we are intentionally using NHDPlusV2 instead of NHDPlusHR because
+# of computational time within the scope of this workflow. For HUC4s that are not
+# included in the NHDPlusV2, we access the NHD Best Resolution product directly
+# from the NHD from the National Map url.
 
 # create list of targets to perform this task
 a_Calculate_Centers_list <- list(
-  # get {sf}s for all US states and territories from {tigris}
+  tar_target(
+    name = a_check_dir_structure,
+    command = {
+      # make directories if needed
+      directories = c("a_Calculate_Centers/mid/",
+                      "a_Calculate_Centers/nhd/",
+                      "a_Calculate_Centers/out/")
+      walk(directories, function(dir) {
+        if(!dir.exists(dir)){
+          dir.create(dir)
+        }
+      })
+    },
+    cue = tar_cue("always"),
+    deployment = "main"
+    ),
+  
+  # get {sf}s for all US states and territories from {tigris} to grab all the HUC4s
   tar_target(
     name = US_states_territories,
     command = states() %>% st_make_valid(),
@@ -42,127 +52,69 @@ a_Calculate_Centers_list <- list(
     pattern = map(US_states_territories)
   ),
   
-  # get distinct HUC4s as a list
+  # get distinct HUC4s as a list from the previous target
   tar_target(
     name = HUC4_list,
     command = HUC4_dataframe %>% 
       distinct() %>% 
       pull("huc4"),
-    packages = "tidyverse"
+    packages = "tidyverse",
+    deployment = "main"
   ),
   
-  # make an empty text file to store empty NHD Plus HR files when they come up 
-  # in the all_poi_points target.
+  # now filter out for HUC4s that are in CONUS (and have NHDPlusV2 waterbodies)
   tar_target(
-    name = make_empty_huc_file,
-    command = write_lines("", file.path("a_Calculate_Centers/out/",
-                                        "empty_hucs.txt")),
-    packages = "readr"
+    name = CONUS_HUC4,
+    command = HUC4_list[as.numeric(HUC4_list) < 1900],
+    deployment = "main"
   ),
   
-  # for each HUC4, download the NHDPlusHR waterbody file, subset to lakes/res/
+  # for each HUC4, grab the NHDPlus waterbodies, subset to lakes/res/
   # impoundments, subset to >= 1ha, and calculate POI for each polygon
-  # run time for this target is > 3 h
+  # run time for this target is ~ 45 min - 1 hour
   tar_target(
-    name = all_poi_points,
-    command = calculate_centers_HUC4(HUC4_list),
-    packages = c("nhdplusTools", "sf", "tidyverse", "polylabelr"),
-    pattern = map(HUC4_list)
-  ),
-  
-  # we'll track the empty hucs file now that it's not empty!
-  tar_file_read(
-    name = empty_hucs_file,
+    name = CONUS_poi,
     command = {
-      all_poi_points
-      "a_Calculate_Centers/out/empty_hucs.txt"
-    },
-    read = read_lines(!!.x),
-    packages = "readr"
-  ),
-  
-  # collate the csv's into a single feather file for use in pull
-  tar_target(
-    name = collated_poi_points,
-    command = {
-      all_poi_points
-      list.files("a_Calculate_Centers/mid/", full.names = T) %>% 
-        map_dfr(., 
-                function(file) { 
-                  read_csv(file, 
-                           col_types = cols(.default = 'c')) # coerce all cols to character
-                  }) %>% 
-        write_feather(., file.path("a_Calculate_Centers/out/",
-                                   "NHDPlusHR_POI_center_locs.feather"))
-    },
-    packages = c("tidyverse", "feather")
-  ),
-  
-  # and let's load/track that file
-  tar_file_read(
-    name = NHDHR_poi_points,
-    command = {
-      collated_poi_points
-      "a_Calculate_Centers/out/NHDPlusHR_POI_center_locs.feather"
+      # need to make sure that the directory structure has been created prior
+      # to running this target
+      a_check_dir_structure
+      calculate_centers_HUC4(CONUS_HUC4)
       },
-    read = read_feather(!!.x),
-    packages = "feather"
-  ), 
-  
-  # most of Alaska is not available in NHDPlusHR. Because of this, we will ditch
-  # all NHDPlusHR data in AK (HUC04s that start with 19) and then download the
-  # NHD Best Resolution file for AK from the National Map. 
-  
-  # first, drop the AK lakes
-  tar_target(
-    name = NHDHR_poi_points_noAK,
-    command = {
-      #make a list of incomplete VPUIDs
-      incomplete_list <- unique(NHDHR_poi_points$VPUID) %>% 
-        # there are a few HUC8s in Alaska with data that we want to drop, these
-        # are indicated by VPUIDs that are 8 characters - and there are a few that
-        # were concatenated in a previous step, resulting in 18 characters. All 
-        # other VPUIDs are either 4, 5, 6, 10, or 11 characters in length.
-        .[nchar(.) == 8 | nchar(.) == 18]
-      NHDHR_poi_points %>% 
-        filter(!VPUID %in% incomplete_list)  
-    },
-    packages = "tidyverse"
+    packages = c("nhdplusTools", "sf", "tidyverse", "polylabelr", "rmapshaper"),
+    pattern = map(CONUS_HUC4)
   ),
   
-  # now download the AK NHD Best Resolution file from the National Map 
-  # and calculate POI for each WBD
+  # and then grab the non-CONUS HUC4s
+  tar_target(
+    name = nonCONUS_HUC4,
+    command = HUC4_list[as.numeric(HUC4_list) >= 1900],
+    deployment = "main"
+  ),
+  
+  # now download the NHD Best Resolution file from the National Map, filter
+  # waterbodies, and calculate POIs
   # run time for this target is > 1h
   tar_target(
-    name = make_AK_poi_points,
-    command = calculate_AK_poi(),
-    packages = c("tidyverse", "sf", "polylabelr", "feather")
+    name = nonCONUS_poi,
+    command = calculate_bestres_centers(nonCONUS_HUC4),
+    packages = c("tidyverse", "sf", "polylabelr"),
+    pattern = map(nonCONUS_HUC4)
   ), 
   
-  # and load/track the resulting file
-  tar_file_read(
-    name = AK_poi_points,
-    command = {
-      make_AK_poi_points
-      "a_Calculate_Centers/out/AK_NHD_BestRes_POI_center_locs.feather"
-      },
-    read = read_feather(!!.x),
-    packages = "feather"
-  ),
-
-  # and now we'll join together the two POI files
+  # and now we'll join together the two POI files, retaining source information
+  # and the unique identifier from NHD
   tar_target(
-    name = combined_poi_file,
-    command = collate_NHDHR_AK(NHDHR_poi_points_noAK,
-                               AK_poi_points),
-    packages = c("tidyverse", "feather")
-  ),
-  
-  # and track/load that file
-  tar_file_read(
-    name = combined_poi_points,
-    command = combined_poi_file,
-    read = read_feather(!!.x),
-    packages = "feather"
+    name = combined_poi,
+    command = {
+      CONUS <- CONUS_poi %>% 
+        mutate(nhd_source = "NHDPlusv2")
+      nonCONUS <- nonCONUS_poi %>% 
+        mutate(nhd_source = "NHDBestRes")
+      full_join(CONUS, nonCONUS) %>% 
+        mutate(nhd_id = if_else(!is.na(comid), comid, permanent_identifier)) %>% 
+        select(-c(comid, permanent_identifier))
+    },
+    packages = c("tidyverse", "feather"),
+    deployment = "main"
   )
 )
