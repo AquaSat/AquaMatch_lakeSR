df34e64e22f432e2b447b422bc72278abcba0867
B Steele <B.Steele@colostate.edu>
2024-11-21 13:44:38 -0700

    add `a_` prefix to targets in the -a- group
    Co-authored-by: Matt Brousil <mbrousil@users.noreply.github.com>

--- a_Calculate_Centers.R
+++ a_Calculate_Centers.R
@@ -35,7 +35,7 @@ a_Calculate_Centers_list <- list(
   
   # get {sf}s for all US states and territories from {tigris} to grab all the HUC4s
   tar_target(
-    name = US_states_territories,
+    name = a_US_states_territories,
     command = states() %>% st_make_valid(),
     packages = c("tigris", "sf", "tidyverse")
     ),
@@ -45,17 +45,17 @@ a_Calculate_Centers_list <- list(
   # this is a good framework to break up HUC acquisition. If you run on the 
   # boundary of the US, this will completely fail and timeout.
   tar_target(
-    name = HUC4_dataframe,
-    command = get_huc(US_states_territories, type = "huc04") %>% 
+    name = a_HUC4_dataframe,
+    command = get_huc(a_US_states_territories, type = "huc04") %>% 
       st_drop_geometry(),
     packages = c("nhdplusTools", "sf", "tidyverse"),
-    pattern = map(US_states_territories)
+    pattern = map(a_US_states_territories)
   ),
   
   # get distinct HUC4s as a list from the previous target
   tar_target(
-    name = HUC4_list,
-    command = HUC4_dataframe %>% 
+    name = a_HUC4_list,
+    command = a_HUC4_dataframe %>% 
       pull("huc4") %>% 
       unique(),
     packages = "tidyverse",
@@ -64,8 +64,8 @@ a_Calculate_Centers_list <- list(
   
   # now filter out for HUC4s that are in CONUS (and have NHDPlusV2 waterbodies)
   tar_target(
-    name = CONUS_HUC4,
-    command = HUC4_list[as.numeric(HUC4_list) < 1900],
+    name = a_CONUS_HUC4,
+    command = a_HUC4_list[as.numeric(a_HUC4_list) < 1900],
     deployment = "main"
   ),
   
@@ -73,21 +73,21 @@ a_Calculate_Centers_list <- list(
   # impoundments, subset to >= 1ha, and calculate POI for each polygon
   # run time for this target is ~ 45 min - 1 hour
   tar_target(
-    name = CONUS_poi,
+    name = a_CONUS_poi,
     command = {
       # need to make sure that the directory structure has been created prior
       # to running this target
       a_check_dir_structure
-      calculate_centers_HUC4(CONUS_HUC4)
+      calculate_centers_HUC4(a_CONUS_HUC4)
       },
     packages = c("nhdplusTools", "sf", "tidyverse", "polylabelr", "rmapshaper"),
-    pattern = map(CONUS_HUC4)
+    pattern = map(a_CONUS_HUC4)
   ),
   
   # and then grab the non-CONUS HUC4s
   tar_target(
-    name = nonCONUS_HUC4,
-    command = HUC4_list[as.numeric(HUC4_list) >= 1900],
+    name = a_nonCONUS_HUC4,
+    command = a_HUC4_list[as.numeric(a_HUC4_list) >= 1900],
     deployment = "main"
   ),
   
@@ -95,27 +95,27 @@ a_Calculate_Centers_list <- list(
   # waterbodies, and calculate POIs
   # run time for this target is > 1h
   tar_target(
-    name = nonCONUS_poi,
+    name = a_nonCONUS_poi,
     command = {
       # need to make sure that the directory structure has been created prior
       # to running this target
       a_check_dir_structure
-      calculate_bestres_centers(nonCONUS_HUC4)
+      calculate_bestres_centers(a_nonCONUS_HUC4)
       },
     packages = c("tidyverse", "sf", "polylabelr"),
-    pattern = map(nonCONUS_HUC4)
+    pattern = map(a_nonCONUS_HUC4)
   ), 
   
   # and now we'll join together the two POI files, retaining source information
   # and the unique identifier from NHD
   tar_target(
-    name = combined_poi,
+    name = a_combined_poi,
     command = {
-      CONUS <- CONUS_poi %>% 
+      CONUS <- a_CONUS_poi %>% 
         mutate(nhd_source = "NHDPlusv2")
-      nonCONUS <- nonCONUS_poi %>% 
+      nonCONUS <- a_nonCONUS_poi %>% 
         mutate(nhd_source = "NHDBestRes")
-      full_join(CONUS, nonCONUS) %>% 
+      full_join(a_CONUS, a_nonCONUS) %>% 
         mutate(nhd_id = if_else(!is.na(comid), comid, permanent_identifier)) %>% 
         select(-c(comid, permanent_identifier))
     },
