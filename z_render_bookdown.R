2ae843e99ffafc5bbab4a0b05c76271fb10b8626
B Steele <B.Steele@colostate.edu>
2024-11-21 08:54:21 -0700

    add some bookdown details, fix bookdown knit, add WI example

--- z_render_bookdown.R
+++ z_render_bookdown.R
@@ -1,12 +1,12 @@
 z_render_bookdown <- list(
   tar_target(name = render_bookdown,
              command = {
-               poi_tasks_complete #this should always be the last target!
+               #poi_tasks_complete #this should always be the last target!
                render_book(input = "bookdown/",
                            params = list(
-                             all_pts = combined_poi_points,
-                             ak_pts = AK_poi_points
+                             poi = combined_poi
                            ))
              },
-             packages = "bookdown")
+             packages = c("tidyverse", "bookdown", "sf", "tigris", "nhdplusTools", "tmap"),
+             deployment = "main")
 )
