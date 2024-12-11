b12ddc300e07f4055538f586a87617ce5eae6bd0
B Steele <B.Steele@colostate.edu>
2024-12-11 12:53:35 -0700

    move files from main to new_adds for PR

--- a_Calculate_Centers/src/calculate_centers_HUC4.R
+++ a_Calculate_Centers/src/calculate_centers_HUC4.R
@@ -37,7 +37,7 @@ calculate_centers_HUC4 <- function(HUC4) {
     # grab smaller (<4ha) lakes/ponds that are characterized as intermittent by NHD
     intermittent <- wbd_filter %>% 
       filter(areasqkm < 0.04,
-             fcode %in% c(39001, 39005))
+             fcode %in% c(39001, 39005, 39006, 43614))
     # and remove from dataset for processing time
     wbd_filter <- wbd_filter %>% filter(!comid %in% intermittent$comid)
     
@@ -140,7 +140,7 @@ calculate_centers_HUC4 <- function(HUC4) {
       poi_sf_trans <- st_transform(poi_sf, st_crs(wbd_valid))
       # filter for containment
       contained_poi <- poi_sf_trans[wbd_valid, ]
-
+      
       # and now grab the poi lat/lon from the poi_df and drop geometry
       poi <- contained_poi %>%
         left_join(., poi_df) %>% 
