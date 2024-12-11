c0e274f9aac3cf55401835f7464f4dc46056c920
B Steele <B.Steele@colostate.edu>
2024-12-11 12:52:22 -0700

    more tweaks to bookdown

--- z_render_bookdown.R
+++ z_render_bookdown.R
@@ -6,16 +6,20 @@ z_render_bookdown <- list(
            command = "bookdown/01-Introduction.Rmd"),
   tar_file(name = locations,
            command = "bookdown/02-Data_Acquisition_Locations.Rmd"),
-  # render bookdwon, 
+  tar_file(name = refs,
+           command = "bookdown/z-Refs.Rmd"),
+  # render bookdown, add req's of the above files in command prompt 
   tar_target(name = render_bookdown,
              command = {
                index
                intro
                locations
+               refs
                #poi_tasks_complete #this should always be the last target!
                render_book(input = "bookdown/",
                            params = list(
-                             poi = a_combined_poi
+                             poi = a_combined_poi,
+                             locs_run_date = "November 2024"
                            ))
              },
              packages = c("tidyverse", "bookdown", "sf", "tigris", "nhdplusTools", "tmap"),
