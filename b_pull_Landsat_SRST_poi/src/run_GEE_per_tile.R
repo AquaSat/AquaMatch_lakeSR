066471f38d584651be8e7f97a00d0a3553ce27fa
B Steele <B.Steele@colostate.edu>
2024-12-12 14:03:36 -0700

    move -b- group to new_adds

--- b_pull_Landsat_SRST_poi/src/run_GEE_per_tile.R
+++ b_pull_Landsat_SRST_poi/src/run_GEE_per_tile.R
@@ -1,16 +1,17 @@
-#' @title Run GEE script per tile
+#' @title Run GEE script per Landsat path-row
 #' 
 #' @description
-#' Function to run the Landsat Pull for a specified WRS2 tile.
+#' Function to run the Landsat pull for a specified WRS2 path-row
 #' 
-#' @param WRS_tile tile to run the GEE pull on
-#' @returns Silently writes a text file of the current tile (for use in the
-#' Python script). Silently triggers GEE to start stack acquisition per tile.
+#' @param WRS_pathrow Landsat path-row to run the GEE pull on
+#' @returns Silently writes a text file of the current path-row (for use in the
+#' Python script). Silently triggers GEE to start stack acquisition for that 
+#' path-row.
 #' 
 #' 
-run_GEE_per_tile <- function(WRS_tile) {
+run_GEE_per_pathrow <- function(WRS_pathrow) {
   # document WRS tile for python script
-  write_lines(WRS_tile, "b_pull_Landsat_SRST_poi/out/current_tile.txt", sep = "")
+  write_lines(WRS_pathrow, "b_pull_Landsat_SRST_poi/out/current_pathrow.txt", sep = "")
   # run the python script
-  source_python("b_pull_Landsat_SRST_poi/py/runGEEperTile.py")
+  source_python("b_pull_Landsat_SRST_poi/py/runGEEperPathRow.py")
 }
