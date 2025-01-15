4e66f9fba2db2e321ca35bad625ffb153588be4a
B Steele <B.Steele@colostate.edu>
2025-01-15 16:20:21 -0700

    work on bookdown

--- _targets.R
+++ _targets.R
@@ -34,12 +34,42 @@ tar_option_set(
 
 poi_config <- "b_pull_Landsat_SRST_poi/config_files/config_poi.yml"
 
+
+# Create configuration targets:  ------------------------------------------
+
+config_list = list(
+  
+  # Grab location of the local {targets} siteSR pipeline OR error if
+  # the location doesn't exist yet
+  tar_target(
+    name = config_siteSR_directory,
+    # command = if(dir.exists(p0_siteSR_config$harmonize_repo_directory)) {
+    #   p0_siteSR_config$harmonize_repo_directory
+    # } else {
+    #   # Throw an error if the pipeline does not exist
+    #   stop("The WQP download pipeline is not at the location specified in the 
+    #        config.yml file. Check the location specified as `harmonize_repo_directory`
+    #        in the config.yml file and rerun the pipeline.")
+    # },
+    command = if(dir.exists("../AquaMatch_siteSR_WQP/")) {
+      "../AquaMatch_siteSR_WQP/"
+    } else {
+      # Throw an error if the pipeline does not exist
+      stop("The siteSR pipeline is not located at `../AquaMatch_siteSR_WQP/` and 
+           the pipeline can not continue.")
+    },
+    cue = tar_cue("always")
+  )
+
+)
+
 # Source targets groups: ---------------------------------------
 
 tar_source(files = c(
   "a_Calculate_Centers.R",
   "b_pull_Landsat_SRST_poi.R",
   "c_collate_Landsat_data.R",
+  "y_siteSR_targets.R",
   "z_render_bookdown.R"
   )
 )
@@ -47,8 +77,10 @@ tar_source(files = c(
 # Collate targets groups: ---------------------------------------
 
 list(
+  config_list,
   a_Calculate_Centers_list,
   b_pull_Landsat_SRST_poi_list,
   c_collate_Landsat_data,
+  y_siteSR_list,
   z_render_bookdown
 )
---
+++ y_siteSR_targets.R
@@ -0,0 +1,33 @@
+tar_source("src/")
+
+y_siteSR_list <- list(
+  
+  tar_target(
+    name = y_siteSR_targets,
+    command = read_csv(file.path(config_siteSR_directory, 
+                                 "99_compile_drive_ids/out/",
+                                 "target_drive_ids.csv")),
+    cue = tar_cue("always")
+  ),
+  
+  tar_target(
+    name = p4_WQP_site_NHD_info,
+    command = retrieve_data(target = "p4_WQP_site_NHD_info",
+                            id_df = y_siteSR_targets,
+                            local_folder = tempdir(),
+                            stable = FALSE,
+                            google_email = b_yml_poi$google_email),
+    packages = c("tidyverse", "googledrive")
+  ),
+  
+  tar_target(
+    name = p5_visible_sites,
+    command = retrieve_data(target = "p5_visible_sites",
+                            id_df = y_siteSR_targets,
+                            local_folder = tempdir(),
+                            stable = FALSE,
+                            google_email = b_yml_poi$google_email),
+    packages = c("tidyverse", "googledrive")
+  )
+  
+)
