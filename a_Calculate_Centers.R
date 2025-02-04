5765de11cfdd788ea0f43ee17a22920eb9db9108
B Steele <B.Steele@colostate.edu>
2025-02-04 10:40:34 -0700

    implement configuration for centers calculation

--- a_Calculate_Centers.R
+++ a_Calculate_Centers.R
@@ -1,3 +1,4 @@
+
 # Source functions for this {targets} list
 tar_source("a_Calculate_Centers/src/")
 
@@ -14,106 +15,189 @@ tar_source("a_Calculate_Centers/src/")
 # included in the NHDPlusV2, we access the NHD Best Resolution product directly
 # from the NHD from the National Map url.
 
-# create list of targets to perform this task
-a_Calculate_Centers_list <- list(
-  tar_target(
-    name = a_check_dir_structure,
-    command = {
-      # make directories if needed
-      directories = c("a_Calculate_Centers/mid/",
-                      "a_Calculate_Centers/nhd/",
-                      "a_Calculate_Centers/out/")
-      walk(directories, function(dir) {
-        if(!dir.exists(dir)){
-          dir.create(dir)
-        }
-      })
-    },
-    cue = tar_cue("always")
-  ),
+# check for configuration setting to calculate centers, if TRUE, calculate centers
+if (config::get(config = "admin_update")$calculate_centers) {
   
-  # get {sf}s for all US states and territories from {tigris} to grab all the HUC4s
-  tar_target(
-    name = a_US_states_territories,
-    command = states() %>% st_make_valid(),
-    packages = c("tigris", "sf", "tidyverse")
-  ),
-  
-  # for each state/territory, get a list of HUC4s
-  # while this is not the most efficient (HUC4s cross state boundaries),
-  # this is a good framework to break up HUC acquisition. If you run on the 
-  # boundary of the US, this will completely fail and timeout.
-  tar_target(
-    name = a_HUC4_dataframe,
-    command = get_huc(a_US_states_territories, type = "huc04") %>% 
-      st_drop_geometry(),
-    packages = c("nhdplusTools", "sf", "tidyverse"),
-    pattern = map(a_US_states_territories)
-  ),
-  
-  # get distinct HUC4s as a list from the previous target
-  tar_target(
-    name = a_HUC4_list,
-    command = a_HUC4_dataframe %>% 
-      pull("huc4") %>% 
-      unique()
-  ),
-  
-  # now filter out for HUC4s that are in CONUS (and have NHDPlusV2 waterbodies)
-  tar_target(
-    name = a_CONUS_HUC4,
-    command = a_HUC4_list[as.numeric(a_HUC4_list) < 1900]
-  ),
-  
-  # for each HUC4, grab the NHDPlus waterbodies, subset to lakes/res/
-  # impoundments, subset to >= 1ha, and calculate POI for each polygon
-  # run time for this target is ~ 45 min - 1 hour
-  tar_target(
-    name = a_CONUS_poi,
-    command = {
-      # need to make sure that the directory structure has been created prior
-      # to running this target
-      a_check_dir_structure
-      calculate_centers_HUC4(a_CONUS_HUC4)
-    },
-    packages = c("nhdplusTools", "sf", "tidyverse", "polylabelr", "rmapshaper"),
-    pattern = map(a_CONUS_HUC4)
-  ),
-  
-  # and then grab the non-CONUS HUC4s
-  tar_target(
-    name = a_nonCONUS_HUC4,
-    command = a_HUC4_list[as.numeric(a_HUC4_list) >= 1900]
-  ),
-  
-  # now download the NHD Best Resolution file from the National Map, filter
-  # waterbodies, and calculate POIs
-  # run time for this target is > 1h
-  tar_target(
-    name = a_nonCONUS_poi,
-    command = {
-      # need to make sure that the directory structure has been created prior
-      # to running this target
-      a_check_dir_structure
-      calculate_bestres_centers(a_nonCONUS_HUC4)
-    },
-    packages = c("tidyverse", "sf", "polylabelr"),
-    pattern = map(a_nonCONUS_HUC4)
-  ), 
-  
-  # and now we'll join together the two POI files, retaining source information
-  # and the unique identifier from NHD
-  tar_target(
-    name = a_combined_poi,
-    command = {
-      CONUS <- a_CONUS_poi %>% 
-        mutate(nhd_source = "NHDPlusv2")
-      nonCONUS <- a_nonCONUS_poi %>% 
-        mutate(nhd_source = "NHDBestRes")
-      full_join(CONUS, nonCONUS) %>% 
-        mutate(nhd_id = if_else(!is.na(comid), comid, permanent_identifier)) %>% 
-        select(-c(comid, permanent_identifier))
-    },
-    packages = c("tidyverse", "feather")
+  # create list of targets to perform this task
+  a_Calculate_Centers_list <- list(
+    tar_target(
+      name = a_check_dir_structure,
+      command = {
+        # make directories if needed
+        directories = c("a_Calculate_Centers/mid/",
+                        "a_Calculate_Centers/nhd/",
+                        "a_Calculate_Centers/out/")
+        walk(directories, function(dir) {
+          if(!dir.exists(dir)){
+            dir.create(dir)
+          }
+        })
+      },
+      cue = tar_cue("always")
+    ),
+    
+    # get {sf}s for all US states and territories from {tigris} to grab all the HUC4s
+    tar_target(
+      name = a_US_states_territories,
+      command = states() %>% st_make_valid(),
+      packages = c("tigris", "sf", "tidyverse")
+    ),
+    
+    # for each state/territory, get a list of HUC4s
+    # while this is not the most efficient (HUC4s cross state boundaries),
+    # this is a good framework to break up HUC acquisition. If you run on the 
+    # boundary of the US, this will completely fail and timeout.
+    tar_target(
+      name = a_HUC4_dataframe,
+      command = get_huc(a_US_states_territories, type = "huc04") %>% 
+        st_drop_geometry(),
+      packages = c("nhdplusTools", "sf", "tidyverse"),
+      pattern = map(a_US_states_territories)
+    ),
+    
+    # get distinct HUC4s as a list from the previous target
+    tar_target(
+      name = a_HUC4_list,
+      command = a_HUC4_dataframe %>% 
+        pull("huc4") %>% 
+        unique()
+    ),
+    
+    # now filter out for HUC4s that are in CONUS (and have NHDPlusV2 waterbodies)
+    tar_target(
+      name = a_CONUS_HUC4,
+      command = a_HUC4_list[as.numeric(a_HUC4_list) < 1900]
+    ),
+    
+    # for each HUC4, grab the NHDPlus waterbodies, subset to lakes/res/
+    # impoundments, subset to >= 1ha, and calculate POI for each polygon
+    # run time for this target is ~ 45 min - 1 hour
+    tar_target(
+      name = a_CONUS_poi,
+      command = {
+        # need to make sure that the directory structure has been created prior
+        # to running this target
+        a_check_dir_structure
+        calculate_centers_HUC4(a_CONUS_HUC4)
+      },
+      packages = c("nhdplusTools", "sf", "tidyverse", "polylabelr", "rmapshaper"),
+      pattern = map(a_CONUS_HUC4)
+    ),
+    
+    # and then grab the non-CONUS HUC4s
+    tar_target(
+      name = a_nonCONUS_HUC4,
+      command = a_HUC4_list[as.numeric(a_HUC4_list) >= 1900]
+    ),
+    
+    # now download the NHD Best Resolution file from the National Map, filter
+    # waterbodies, and calculate POIs
+    # run time for this target is > 1h
+    tar_target(
+      name = a_nonCONUS_poi,
+      command = {
+        # need to make sure that the directory structure has been created prior
+        # to running this target
+        a_check_dir_structure
+        calculate_bestres_centers(a_nonCONUS_HUC4)
+      },
+      packages = c("tidyverse", "sf", "polylabelr"),
+      pattern = map(a_nonCONUS_HUC4)
+    ), 
+    
+    # and now we'll join together the two POI files, retaining source information
+    # and the unique identifier from NHD
+    tar_target(
+      name = a_combined_poi,
+      command = {
+        CONUS <- a_CONUS_poi %>% 
+          mutate(nhd_source = "NHDPlusv2")
+        nonCONUS <- a_nonCONUS_poi %>% 
+          mutate(nhd_source = "NHDBestRes")
+        full_join(CONUS, nonCONUS) %>% 
+          mutate(nhd_id = if_else(!is.na(comid), comid, permanent_identifier)) %>% 
+          select(-c(comid, permanent_identifier))
+      }
+    ),
+    
+    # check for drive folder if set to export
+    tar_target(
+      name = a_check_Drive_poi_folder,
+      command = {
+        b_check_Drive_parent_folder
+        tryCatch({
+          drive_auth(lakeSR_config$google_email)
+          folder_name = "lakeSR_combined_poi"
+          if (b_yml_poi$parent_folder != "") {
+            path <- file.path("~",
+                              b_yml_poi$parent_folder, 
+                              folder_name)
+          } else {
+            path <- folder_name
+          }
+          drive_ls(path, type = "folder")
+          return(path)
+        }, error = function(e) {
+          if (b_yml_poi$parent_folder != "") {
+            drive_mkdir(name = folder_name,
+                        path = b_yml_poi$parent_folder)
+          } else {
+            drive_mkdir(name = folder_name)
+          }
+          return(path)
+        })
+      },
+      packages = "googledrive",
+      cue = tar_cue("always")
+    ),
+    
+    # send poi file to drive 
+    tar_target(
+      name = a_send_combined_poi_to_drive,
+      command = export_single_target(
+        target = a_combined_poi, 
+        target_name = "a_combined_poi",
+        drive_path = a_check_Drive_poi_folder,
+        google_email = lakeSR_config$google_email,
+        date_stamp = lakeSR_config$centers_version),
+      packages = c("tidyverse", "googledrive")
+    ),
+    
+    # get drive_ids, store
+    tar_target(
+      name = a_save_combined_poi_info,
+      command = {
+        drive_ids <- a_send_combined_poi_to_drive %>% 
+          select(name, id)
+        write_csv(drive_ids,
+                  "a_Calculate_Centers/out/combined_poi_drive_ids.csv")
+        drive_ids
+      },
+      packages = c("tidyverse", "googledrive")
+    )
+    
   )
-)
+  
+} else {
+  
+  # if the run is not set up to calculate centers, just store the combined poi 
+  # data from a previous run, using the drive ids and the general configuration 
+  # file for this repo
+  
+  a_Calculate_Centers_list <- list(
+    
+    tar_target(
+      name = a_combined_poi,
+      command = retrieve_target(
+        target = "a_combined_poi", 
+        id_df = read_csv("a_Calculate_Centers/out/combined_poi_drive_ids.csv"), 
+        local_folder = "a_Calculate_Centers/mid/", 
+        google_email = lakeSR_config$google_email, 
+        version_date = lakeSR_config$centers_version
+      ),
+      packages = c("tidyverse", "googledrive")
+    )
+    
+  )
+  
+}
--- src/export_single_target.R
+++ src/export_single_target.R
@@ -4,29 +4,29 @@
 #' A function to export a single target (as a file) to Google Drive and return
 #' the shareable Drive link as a file path.
 #' 
-#' @param target The name of the target to be exported (as an object not a string).
+#' @param target Thetarget to be exported (as an object not a string).
+#' 
+#' @param target_name The name of the target to be exported (as a string, not an object).
 #' 
 #' @param drive_path A path to the folder on Google Drive where the file
 #' should be saved.
 #' 
-#' @param stable Logical value. If TRUE, also export the file to the "stable"
-#' subfolder in Google Drive. If FALSE, use the path as provided by the user.
-#' 
 #' @param google_email A string containing the gmail address to use for
 #' Google Drive authentication.
 #' 
+#' @param date_stamp A string to version the upload by. default is NULL
+#'
 #' @param feather Logical value. If TRUE, export the file as a feather file. If
 #' FALSE, then ".rds". Defaults to FALSE.
 #' 
 #' @returns 
-#' Returns a local path to a csv file containing a text link to the uploaded
-#' file in Google Drive.
+#' The contents of the folder indicated in the `drive_path` argument.
 #' 
-export_single_file <- function(target, drive_path, stable, google_email,
-                               date_stamp, feather = FALSE){
+export_single_target <- function(target, target_name, drive_path, stable, google_email,
+                               date_stamp = NULL, feather = FALSE){
   
   # Feather or RDS?
-  if(feather){extension <- ".feather"} else {extension <- ".rds"}
+  if (feather) {extension <- ".feather"} else {extension <- ".rds"}
   
   # Authorize using the google email provided
   drive_auth(google_email)
@@ -47,42 +47,28 @@ export_single_file <- function(target, drive_path, stable, google_email,
               file = file_local_path)
   }
   
+  filename <- if (!is.null(date_stamp)) {
+    paste0(target_string, "_v", date_stamp, extension)
+  } else {
+    paste0(target_string, extension)
+  }
+  
   # Once locally exported, send to Google Drive
   out_file <- drive_put(media = file_local_path,
                         # The folder on Google Drive
                         path = drive_path,
                         # The filename on Google Drive
-                        name = paste0(target_string, extension))
+                        name = filename)
   
   # Make the Google Drive link shareable: anyone can view
   drive_share_anyone(out_file)
   
-  # If stable == TRUE then export a second, dated file to the stable/ subfolder
-  if(stable){
-    drive_path_stable <- paste0(drive_path, "stable/")
-    
-    # Once locally exported, send to Google Drive
-    out_file_stable <- drive_upload(media = file_local_path,
-                                    # The folder on Google Drive
-                                    path = drive_path_stable,
-                                    # The filename on Google Drive
-                                    name = paste0(target_string,
-                                                  "_",
-                                                  gsub(pattern = "-",
-                                                       replacement = "",
-                                                       x = date_stamp),
-                                                  extension),
-                                    # Error if file exists with same date
-                                    # Note that we don't do this before this
-                                    # instance because files will always have the
-                                    # same name: no dates attached unless "stable"
-                                    overwrite = FALSE)
-    
-    # Make the Google Drive link shareable: anyone can view
-    drive_share_anyone(out_file_stable)
-  }
-  
   # Now remove the local file after upload is complete
   file.remove(file_local_path)
   
+  # return the contents of the drive path
+  drive_ls(drive_path) %>% 
+    filter(grepl(target_name, name))
+  
 }
+
